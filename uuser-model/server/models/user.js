4cb5e8cfaf8d176d46c6294e689d1f1eeac3c38a
jirayuch01 <jirayu_ch01@hotmail.com>
2017-09-19 14:01:07 +0700

    test
    test

---
+++ uuser-model/server/models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+// {
+//   email: 'andrew@example.com',
+//   password: 'adpsofijasdfmpoijwerew',
+//   tokens: [{
+//     access: 'auth',
+//     token: 'poijasdpfoimasdpfjiweproijwer'
+//   }]
+// }
+
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+    unique: true
+  }
+});
+
+module.exports = {User}
